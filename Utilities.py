c3d4b83480d539ccbd985a944854e81d055abaf8
Alekos Filini <alekos.filini@gmail.com>
2017-10-07 17:22:30 +0200

    Aggiunte utilities

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,12 @@
 def string_to_byte(s):
 	return [int(x, 16) for x in map(''.join, zip(*[iter(s)]*2))]
 
+def string_to_chars_values(s):
+	return [ord(x) for x in list(s)]
+
+def nfc_response_to_array(resp):
+	return string_to_byte(resp.replace(' ', ''))
+
 def toUint(dataB):
   return int(bytes.encode('hex'), 16)
 
@@ -31,7 +37,7 @@ def checkdigit(data):
 	tot = 0
 	weight =  [7, 3, 1]
 	for i in range(0, len(data)):
-		ch = data[i].upper()
+		ch = chr(data[i]).upper()
 		#print ch
 		if ch >= 'A' and ch <= 'Z':
 			curval = ord(ch) - ord('A') + 10
