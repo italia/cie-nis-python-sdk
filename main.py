1cc859852b9c25fb60f78cc8afe967b6703e8c14
Alekos Filini <alekos.filini@gmail.com>
2017-10-08 14:37:07 +0200

    Migliorata interfaccia da linea di comando

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+# coding=utf-8
 from pkg.lib.CIEInterface import CIEInterface
 
 
 def main():
+    print '\n                ██████╗██╗███████╗██████╗ \n' \
+          '               ██╔════╝██║██╔════╝╚════██╗\n' \
+          '               ██║     ██║█████╗   █████╔╝\n' \
+          '               ██║     ██║██╔══╝   ╚═══██╗\n' \
+          '               ╚██████╗██║███████╗██████╔╝\n' \
+          '                ╚═════╝╚═╝╚══════╝╚═════╝ \n'
+
     interface = CIEInterface()
     interface.mrtdAuth('YYMMDD', 'YYMMDD', '*********')
     data = interface.extractData()
 
-    print(data)
+    print 'Nome e cognome: {}\n' \
+          'Codice fiscale: {}\n' \
+          'Residenza: {}\n' \
+          'Luogo di nascita: {}\n' \
+          'Data di nascita: {}' \
+        .format(data['additional_details']['full_name'].replace('<<', ' '),
+                data['additional_details']['vat_code'],
+                data['additional_details']['address'].replace('<', ' '),
+                data['additional_details']['birth_place'].replace('<', ' '),
+                data['additional_details']['birth_date'])
+
 
 if __name__ == "__main__":
     main()
